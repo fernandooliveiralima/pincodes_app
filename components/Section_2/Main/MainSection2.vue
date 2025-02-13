4017d7007d5a96de99c19df64c48115b9e21a669
fernandooliveiralima <oliveirafernando.contato@gmail.com>
2025-02-13 09:35:22 -0300

    feat sessao 2

---
+++ components/Section_2/Main/MainSection2.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex mt-20">
+
+    <section class="">
+      <img src="../../../shared/img/Lead Marketing Template With Mockups Instagram Post (4) 1.png" alt="">
+    </section>
+
+    <section class="flex flex-col">
+      <h1 class="text-[3.4375rem] text-primary-blue-300 font-bold">
+        TRANSFORMANDO IDEIAS EM EXPERIÊNCIAS DIGITAIS <br>
+      </h1>
+      <h3 class="text-[2.75rem] text-[#FFC400]">
+        Movidos por inovação e ambição
+      </h3>
+      <div class="flex flex-col text-3xl text-dark-helper-100 mt-5">
+        <p>
+          Na PinCode, não apenas criamos sistemas web e <br> interfaces, 
+          mas desenvolvemos experiências que conectam <br> usuários
+          e impulsionam negócios. <br>
+        </p>
+        <p class="mt-5">
+          Cada projeto começa com um protótipo bem estruturado, <br> 
+          alinhando design e funcionalidade para garantir soluções
+          eficientes e escaláveis. <br>
+        </p>
+        <p class="mt-5">
+          Da concepção à implementação, entregamos tecnologia <br> sob medida, 
+          projetada para atender às necessidades do seu
+          negócio.
+        </p>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-  import Header from '~/shared/components/Header/Header.vue';
-  import Main from '~/components/Section_1/Main/Main.vue';
+import Header from '~/shared/components/Header/Header.vue';
+import Main from '~/components/Section_1/Main/Main.vue';
+import MainSection2 from '~/components/Section_2/Main/MainSection2.vue';
 </script>
-  
+
 <template>
   <div class="w-full bg-primary-blue-300">
-  <Header/>
-  <Main/>
+
+    <section>
+
+      <Header />
+      <Main />
+
+    </section>
+
+  </div>
+  <div>
+    <MainSection2 />
   </div>
 </template>
-  
-<style scoped>
-  
-</style>
+
+<style scoped></style>
